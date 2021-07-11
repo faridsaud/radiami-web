b24dd412a65192de2665d4741cba6d9cd469aaea
Farid Saud <faridsaud93@gmail.com>
2021-07-11 16:02:26 -0500

    [Feat]: Add media card and shows section

---
+++ src/components/Shared.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PaddedContent = styled.div`
+  width: 100%;
+  background: transparent;
+  @media (min-width: 768px) {
+    max-width: 1440px;
+    margin: auto;
+  }
+`;
