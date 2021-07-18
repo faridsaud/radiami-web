5254211c05e5574ecec372cd899ac9b6b80b0827
Farid Saud <faridsaud93@gmail.com>
2021-07-17 19:43:13 -0500

    [Feat]: Add Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,17 @@ import * as React from "react";
 import Page from "../components/Page";
 import Jumbotron from "../components/Jumbotron";
 import styled from "styled-components";
-import { PaddedContent } from "../components/Shared";
-import TitleCard from "../components/TitleCard";
 import { Shows } from "../components/Shows";
+import ReactPlayer from "react-player";
+import FacebookVideos from "../components/FacebookVideos";
 
 const IndexPage = () => {
   return (
     <Page>
       <Jumbotron />
+      <ReactPlayer url="https://www.facebook.com/lavozdesuamigo/videos/971531730306684/" />
       <Shows />
+
       <Container></Container>
     </Page>
   );
